c158a9ac3426b0a05b2366fd8ff887df5cbc7344
Diksha soni <dikshasonics@gmail.com>
2022-10-01 13:29:14 +0530

    first commit

---
+++ ceramic-tutorial/pages/_app.js
@@ -0,0 +1,13 @@
+import { Provider } from "@self.id/react";
+
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Provider client={{ ceramic: "testnet-clay" }}>
+      <Component {...pageProps} />;
+    </Provider>
+  );
+}
+
+export default MyApp
